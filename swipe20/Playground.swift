39c21d07eac910b76ba28a6ec423408e0aee87e6
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-21 10:11:19 -0700

    animation

--- swipe20/Playground.swift
+++ swipe20/Playground.swift
@@ -7,12 +7,30 @@
 import SwiftUI
 
 struct Playground: View {
-    @State var value = 1
+    @State var value = 0
+    @State var color = Color.green
     var body: some View {
         VStack {
+            Rectangle()
+                .fill(color)
+                .frame(width:100, height:100)
             Text("Hello World \(value)")
             Button("Test") {
-                self.value += 1
+                withAnimation { () -> Void in
+                    self.value += 1
+                    switch(self.value) {
+                    case 1:
+                        self.color = Color.blue
+                    case 2:
+                        self.color = Color.red
+                    case 3:
+                        self.color = Color.yellow
+                    case 4:
+                        self.color = Color.black
+                    default:
+                        self.color = Color.white
+                    }
+                }
             }
         }
     }
