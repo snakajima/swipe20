2426d9b84a80c18b0baaee8a55cc89ae467bcebb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-20 21:27:32 -0700

    Initial Commit

---
+++ swipe20/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swipe20
+//
+//  Created by SATOSHI NAKAJIMA on 9/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
