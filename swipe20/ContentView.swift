a8babdfa9d71554a98658804b2d99ddb8654b9be
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-22 10:25:06 -0700

    SwipeFileView

--- swipe20/ContentView.swift
+++ swipe20/ContentView.swift
@@ -4,42 +4,13 @@
 //
 //  Created by SATOSHI NAKAJIMA on 9/20/20.
 //
-
 import SwiftUI
 
-
 struct ContentView: View {
-    @State var frameIndex = 0
-    let scene:SwipeScene
-    
-    init() {
-        if let path = Bundle.main.path(forResource: "sample1", ofType: "json"),
-           let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
-           let json = try?  JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? [String:Any] {
-            self.scene = SwipeScene(json)
-        } else {
-            self.scene = SwipeScene([:])
-            print("load JSON failed")
-        }
-    }
     var body: some View {
-        VStack {
-            SwipeView(scene:scene, frameIndex:frameIndex)
-            HStack {
-                Button("Prev") {
-                    self.frameIndex -= 1
-                }
-                .disabled(frameIndex <= 0)
-                Button("Next") {
-                    self.frameIndex += 1
-                }
-                .disabled(frameIndex >= scene.frameCount - 1)
-                Text("Frame#: \(self.frameIndex)")
-            }
-        }
+        SwipeFileView(filename:"sample1")
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ swipe20/SwipeFileView.swift
@@ -0,0 +1,46 @@
+//
+//  SwipeFileView.swift
+//  swipe20
+//
+//  Created by SATOSHI NAKAJIMA on 9/22/20.
+//
+import SwiftUI
+
+struct SwipeFileView: View {
+    @State var frameIndex = 0
+    let scene:SwipeScene
+    
+    init(filename:String) {
+        if let path = Bundle.main.path(forResource: filename, ofType: "json"),
+           let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+           let json = try?  JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? [String:Any] {
+            self.scene = SwipeScene(json)
+        } else {
+            self.scene = SwipeScene([:])
+            print("load JSON failed")
+        }
+    }
+
+    var body: some View {
+        VStack {
+            SwipeView(scene:scene, frameIndex:frameIndex)
+            HStack {
+                Button("Prev") {
+                    self.frameIndex -= 1
+                }
+                .disabled(frameIndex <= 0)
+                Button("Next") {
+                    self.frameIndex += 1
+                }
+                .disabled(frameIndex >= scene.frameCount - 1)
+                Text("Frame#: \(self.frameIndex)")
+            }
+        }
+    }
+}
+
+struct SwipeFileView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwipeFileView(filename:"sample1")
+    }
+}
