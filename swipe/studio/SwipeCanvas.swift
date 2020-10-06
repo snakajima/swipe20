491638a91798d94a04d60455c5986617e618949f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-10-06 13:12:16 -0700

    SwipeCanvasModel

--- swipe/studio/SwipeCanvas.swift
+++ swipe/studio/SwipeCanvas.swift
@@ -42,42 +42,6 @@ private let s_script:[String:Any] = [
     ]]
 ]
 
-class SwipeCanvasModel: ObservableObject {
-    @Published var frameIndex = 0 {
-        didSet {
-            selectedElement = nil
-        }
-    }
-    @Published var selectedElement:SwipeElement?
-    @Published var cursorRect:CGRect = .zero
-    @Published var scale = CGPoint(x: 1, y: 1)
-    @Published var isDragging = false
-    @Published var scene:SwipeScene
-    init(scene:SwipeScene) {
-        self.scene = scene
-    }
-
-    var cursorCenter:CGPoint {
-        CGPoint(x: cursorRect.origin.x + cursorRect.width / 2,
-                y: cursorRect.origin.y + cursorRect.height / 2)
-    }
-    
-    var scaledCursor:CGRect {
-        let center = cursorCenter
-        var xf = CGAffineTransform(translationX: center.x, y: center.y)
-        xf = xf.scaledBy(x: scale.x, y: scale.y)
-        xf = xf.translatedBy(x: -center.x, y: -center.y)
-        return cursorRect.applying(xf)
-    }
-    
-    func updateElementFrame(frame:CGRect) {
-        if let element = selectedElement {
-           let updatedElement = element.updated(frame: frame)
-            scene = scene.updated(element: updatedElement, frameIndex: frameIndex)
-        }
-    }
-}
-
 public struct SwipeCanvas: View {
     @ObservedObject var model = SwipeCanvasModel(scene:SwipeScene(s_script))
     public var body: some View {
---
+++ swipe/studio/SwipeCanvasModel.swift
@@ -0,0 +1,44 @@
+//
+//  SwipeCanvasModel.swift
+//  swipe_dev
+//
+//  Created by SATOSHI NAKAJIMA on 10/6/20.
+//
+
+import SwiftUI
+
+class SwipeCanvasModel: ObservableObject {
+    @Published var frameIndex = 0 {
+        didSet {
+            selectedElement = nil
+        }
+    }
+    @Published var selectedElement:SwipeElement?
+    @Published var cursorRect:CGRect = .zero
+    @Published var scale = CGPoint(x: 1, y: 1)
+    @Published var isDragging = false
+    @Published var scene:SwipeScene
+    init(scene:SwipeScene) {
+        self.scene = scene
+    }
+
+    var cursorCenter:CGPoint {
+        CGPoint(x: cursorRect.origin.x + cursorRect.width / 2,
+                y: cursorRect.origin.y + cursorRect.height / 2)
+    }
+    
+    var scaledCursor:CGRect {
+        let center = cursorCenter
+        var xf = CGAffineTransform(translationX: center.x, y: center.y)
+        xf = xf.scaledBy(x: scale.x, y: scale.y)
+        xf = xf.translatedBy(x: -center.x, y: -center.y)
+        return cursorRect.applying(xf)
+    }
+    
+    func updateElementFrame(frame:CGRect) {
+        if let element = selectedElement {
+           let updatedElement = element.updated(frame: frame)
+            scene = scene.updated(element: updatedElement, frameIndex: frameIndex)
+        }
+    }
+}
