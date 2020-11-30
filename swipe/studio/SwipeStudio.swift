9314a4a7516075713a977f18529f2e09b6080b0c
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2020-11-30 06:13:14 -0800

    saves

--- swipe/studio/SwipeStudio.swift
+++ swipe/studio/SwipeStudio.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 #if os(macOS)
 let s_previewHeight:CGFloat = 150
@@ -15,14 +16,23 @@ let s_previewHeight:CGFloat = 100
 
 
 public struct SwipeStudio: View {
-    @State private var scenes = [
+    @State private var scenes = [SwipeScene]()
+    /*
         SwipeScene(s_scriptEmpty),
         SwipeScene(s_scriptGen),
         SwipeScene(s_scriptSample),
-    ]
+    */
     let selectionColor = Color(Color.RGBColorSpace.sRGB, red: 0.0, green: 1.0, blue: 1.0, opacity: 1.0)
     let buttonColor = Color.blue
 
+    init() {
+        print("SwipeStudio init")
+        let moc = PersistenceController.shared.container.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "SceneObject")
+        let sceneObjects = (try? moc.fetch(request) as? [SceneObject]) ?? [SceneObject]()
+        print("sceneObjects", sceneObjects.count)
+    }
+    
     public var body: some View {
         let previewHeight:CGFloat = s_previewHeight
         #if os(macOS)
@@ -50,7 +60,24 @@ public struct SwipeStudio: View {
                     }
                 }
                 Button(action: {
-                    scenes.append(SwipeScene(s_scriptEmpty))
+                    let scene = SwipeScene(s_scriptEmpty)
+                    scenes.append(scene)
+                    guard let data = try? JSONSerialization.data(withJSONObject: scene.script, options: []) else {
+                        print("###ERROR failed to serialize")
+                        return
+                    }
+                    let moc = PersistenceController.shared.container.viewContext
+                    let sceneObject = NSEntityDescription.insertNewObject(forEntityName: "SceneObject", into: moc) as! SceneObject
+                    sceneObject.script = data
+                    sceneObject.createdAt = Date()
+                    sceneObject.updatedAt = sceneObject.createdAt
+                    sceneObject.uuid = scene.uuid
+                    do {
+                        try moc.save()
+                    } catch {
+                        print("###ERROR failed to save", error)
+                    }
+                    
                 }, label: {
                     Text("Add New Scene")
                 })
